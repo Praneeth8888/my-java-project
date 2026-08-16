a8449a536bfdf713d11aeb1fc1434a7557bc374c
Praneeth8888 <hydra.praneeth@gmail.com>
2026-08-16 23:19:27 +0530

    Add new files to existing repository

--- Hello.java
+++ Hello.java
@@ -1,12 +1,13 @@
-import java.util.*;
+import java.util.*; // universal header
 public class Hello{
 
     public static void main(String args[])
     {
 
-        int x ; 
-        Scanner user = new Scanner(System.in);
-        x = user.nextInt();
+        int x ; // variable declaration
+        Scanner user = new Scanner(System.in); // scanner
+
+        x = user.nextInt(); // input
 
         if (x>10 && x<=20){
         System.out.println("Hello, World!");
---
+++ Hello1.java
@@ -0,0 +1,18 @@
+public class Hello1{
+
+    public static void main(String args[])
+    {
+
+        int i=1;
+        while(i<=4){
+            System.out.println("*");
+            i++;
+            int j=1;
+            while(j<=3){
+                System.out.println("#");
+                j++;
+            }
+        }
+        
+    }
+}
