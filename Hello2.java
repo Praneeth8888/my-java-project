ccffefc1dd363cf5f10f56490ac583ba74ab3665
Praneeth8888 <hydra.praneeth@gmail.com>
2026-08-23 22:46:21 +0530

    Added new webpage

--- Hello2.java
+++ Hello2.java
@@ -1,9 +1,22 @@
+//classes , objects
 
+class Calc{ // create class
+    public int add(int n1,int n2)
+    {
+        
+        return n1+n2;
+    }
+}
 
-public Class Hello2{
+public class Hello2{
 
     public static void main(String Args[]){
-
+    int num1=3;
+    int num2= 789;
+ 
+   Calc c = new Calc();// calling the class
+  int result= c.add(num1,num2);
+    System.out.println(result);
 
     }
 }
---
+++ Hello3.java
@@ -0,0 +1,18 @@
+public class Hello3 {
+    public static void main(String[] args) {
+
+        int nums[] = new int[5];
+        nums[0] = 10;
+        nums[1] = 20;   
+        nums[2] = 30;
+        nums[3] = 40;
+        nums[4] = 50;
+
+        for(int i = 0; i <5; i++) {
+            System.out.println(nums[i]);
+        }                   
+        
+
+    }
+
+}
