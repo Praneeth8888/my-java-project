d8394da47a8c99e8e93c846b84d102d57294a1e0
Praneeth8888 <hydra.praneeth@gmail.com>
2026-08-28 00:17:58 +0530

    Strings

---
+++ Hello5.java
@@ -0,0 +1,13 @@
+public class Hello5 {
+    public static void main(String[] args) {
+        String name = new String("Jai Jai");
+
+        System.out.println("Jai Babu ");
+        System.out.println(name.concat(" Babu"));
+
+        String s1="Bob";
+        String s2="Bob";
+
+        System.out.println(s1==s2);
+    }
+}
