097182755034a0b6c682c68e10210cfcc856bc4c
Praneeth8888 <hydra.praneeth@gmail.com>
2026-09-05 00:20:38 +0530

    Calc inheritance

---
+++ Advcalc.java
@@ -0,0 +1,9 @@
+public class Advcalc extends Calculator { //inheritance
+    public int div(int a,int b){
+        return a/b;
+    }
+
+    public int mult(int a,int b){
+        return a*b;
+    }
+}
--- Hello11.java
+++ Hello11.java
@@ -1,20 +1,17 @@
-class Calculator {
-    public int add(int a,int b){
-        return a+b;
-    }
-
-    public int sub(int a,int b){
-        return a-b;
-    }
-}
 
 
 
 
 public class Hello11 {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(5, 3));
-        System.out.println(calc.sub(5, 3));
+        Advcalc calc = new Advcalc();
+       int r1=calc.add(67,69);
+         int r2=calc.sub(67,69);
+         int r3=calc.div(67,69);
+         int r4=calc.mult(67,69);
+          System.out.println("Addition of 67 and 69 is: "+r1);
+          System.out.println("Subtraction of 67 and 69 is: "+r2);
+          System.out.println("Division of 67 and 69 is: "+r3);
+          System.out.println("Multiplication of 67 and 69 is: "+r4);
     }
 }
