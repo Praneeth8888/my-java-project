35813eeaf4becc7bc6b01ec3aa2774bbe0de242f
Praneeth8888 <hydra.praneeth@gmail.com>
2026-09-08 23:38:13 +0530

    Abstract class updated

--- Hello18.java
+++ Hello18.java
@@ -1,6 +1,7 @@
-abstract class car {
-    public abstract void drive();
-        
+abstract class car { // abstract class can have abstract methods only when class is declared as abstract
+    public abstract void drive();//abstract method
+    
+    public abstract void fly();
     
     public void playMusic(){
         System.out.println("Playing music");
@@ -8,19 +9,27 @@ abstract class car {
 
 }
 
-class porsche extends car {
+abstract class porsche extends car {
     public void drive(){
         System.out.println("Driving Porsche");
     }
 }
 
+class Helicopter extends porsche {
+    
+    
+    public void fly(){
+        System.out.println("Flying Helicopter");
+    }
+}
 
 
 public class Hello18{
     public static void main(String[] args){
 
-        car obj = new porsche(); // u cant create object of abstract class
+        car obj = new Helicopter(); // u cant create object of abstract class
         obj.drive();
+        obj.fly();
         obj.playMusic();
         // System.out.println("Hello, World!");
     }
