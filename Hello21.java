4e9440e25fdc40fdd58d912b344b2e936df3822d
Praneeth8888 <hydra.praneeth@gmail.com>
2026-09-14 23:29:27 +0530

    Recap

--- Hello21.java
+++ Hello21.java
@@ -2,22 +2,26 @@ public class Hello21 {
     public static void main(String[] args) {
 
         // -------- Primitive Data Types --------
-        byte b = 100;              // 1 byte
-        short s = 30000;           // 2 bytes
-        int i = 100000;            // 4 bytes
-        long l = 10000000000L;     // 8 bytes
+        byte b = 100;                 // 1 byte
+        short s = 30000;              // 2 bytes
+        int i = 100_000;              // 4 bytes
+        long l = 10_000_000_000L;     // 8 bytes
 
-        float f = 3.14f;           // 4 bytes
-        double d = 3.14159265359;  // 8 bytes
+        float f = 3.14f;              // 4 bytes
+        double d = 3.14159265359;     // 8 bytes
 
-        char c = 'A';              // 2 bytes (Unicode character)
-        boolean flag = true;       // 1 bit
+        char c = 'A';                 // 2 bytes in Java (UTF-16 code unit)
+        boolean flag = true;          // logical value: true or false
 
-        // -------- Non-Primitive Data Types --------
-        String str = "Hello, Java"; // String (class in Java)
-        int[] arr = {1, 2, 3, 4, 5}; // Array
+        // -------- Reference Types / Objects --------
+        String str = "Hello, Java"; // String object
+        int[] arr = {1, 2, 3, 4, 5}; // Array object
         Integer wrapperInt = Integer.valueOf(50); // Wrapper class example
-        StringBuilder sb = new StringBuilder("Java"); // Class object
+        StringBuilder sb = new StringBuilder("Java"); // Mutable string builder
+        String emptyName = null; // Reference type can be null
+
+        // -------- Type Casting Example --------
+        int castedDouble = (int) d; // convert double to int by removing decimal part
 
         // -------- Output --------
         System.out.println("byte: " + b);
@@ -30,6 +34,7 @@ public class Hello21 {
         System.out.println("boolean: " + flag);
 
         System.out.println("String: " + str);
+        System.out.println("Double to int cast: " + castedDouble);
         System.out.print("Array: ");
         for (int num : arr) {
             System.out.print(num + " ");
@@ -38,5 +43,6 @@ public class Hello21 {
 
         System.out.println("Wrapper Integer: " + wrapperInt);
         System.out.println("StringBuilder: " + sb);
+        System.out.println("Null reference example: " + emptyName);
     }
 }
