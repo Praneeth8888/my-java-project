b7d77e9e0c5d93855c9387439a8488dedf28c84d
Praneeth8888 <hydra.praneeth@gmail.com>
2026-09-16 23:53:07 +0530

    Exceptions

---
+++ Hello23.java
@@ -0,0 +1,16 @@
+
+public class Hello23  {
+    public static void main(String a[]) {
+        int i = 0;
+        int j = 0;
+
+        try {
+            j = 18 / i; // Causes ArithmeticException because i is 0
+        } catch(Exception e) {
+            System.out.println("Something Went Wrong.");
+        }
+
+        System.out.println(j);
+        System.out.println("Bye");
+    }
+}
