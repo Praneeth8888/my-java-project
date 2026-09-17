289187dff6fde7984074a9f1af2035b0343d6709
Praneeth8888 <hydra.praneeth@gmail.com>
2026-09-17 23:51:11 +0530

    Exceptions updated

--- Hello23.java
+++ Hello23.java
@@ -1,13 +1,26 @@
 
 public class Hello23  {
     public static void main(String a[]) {
-        int i = 4;
+        int i = 2;
         int j = 0;
+        int nums[]= new int[4];
 
         try {
             j = 18 / i; // Causes ArithmeticException because i is 0
-        } catch(Exception e) {
-            System.out.println("Something Went Wrong.");
+            System.out.println(nums[1]); // This line will not be executed due to the Arithmetic Exception above
+            System.out.println(nums[4]); // This line will also not be executed
+        }
+        
+        catch(ArithmeticException e) {
+            System.out.println("Cannot divide by zero."+ e);
+        }
+
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is out of bounds."+ e);
+        }
+
+        catch(Exception e) {
+            System.out.println("Some other exception occurred."+ e);
         }
 
         System.out.println(j);
