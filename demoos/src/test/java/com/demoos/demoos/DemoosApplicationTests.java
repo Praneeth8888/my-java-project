001ebd09b7d96caa977e96b757758e87afdd394d
Praneeth8888 <hydra.praneeth@gmail.com>
2026-09-24 23:49:15 +0530

    Spring

---
+++ demoos/src/test/java/com/demoos/demoos/DemoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoos.demoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
