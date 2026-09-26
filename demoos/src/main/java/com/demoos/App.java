aec816e12b61b1adf4fa4e0ef159c12f6bde0154
Praneeth8888 <hydra.praneeth@gmail.com>
2026-09-26 23:55:31 +0530

    Spring xml working

--- demoos/src/main/java/com/demoos/App.java
+++ demoos/src/main/java/com/demoos/App.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        dev obj=context.getBean("dev");
+        dev obj = context.getBean("dev", dev.class);
         obj.build();
     }
+}
--- demoos/src/main/java/com/demoos/dev.java
+++ demoos/src/main/java/com/demoos/dev.java
@@ -1,6 +1,17 @@
 package com.demoos;
 
 public class dev {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void build() {
         System.out.println("Building the project...");
     }
